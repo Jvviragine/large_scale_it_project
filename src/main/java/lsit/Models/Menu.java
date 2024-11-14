58e1681ce20c75d114e6a0084ad74459b94fc8b4
LennardTr <115259471+LennardTr@users.noreply.github.com>
2024-11-14 15:39:12 +0100

    Update Menu.java

--- src/main/java/lsit/Models/Menu.java
+++ src/main/java/lsit/Models/Menu.java
@@ -1,5 +1,8 @@
 package lsit.Models;
 
 public class Menu {
+    public MenuItem[] activeItems; //all items on the menu right now
+    public MenuItem[] allItems;    //all possible items once sold
+
     
 }
