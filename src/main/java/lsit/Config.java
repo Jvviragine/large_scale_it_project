fcd8997196c2bee9dbbe28f4912bd041aa6e4c21
João Victor <j.coutinhoviragine@student.maastrichtuniversity.nl>
2024-11-19 11:52:11 +0100

    Added all the Changes from the Previous Live Coding (OpenID Authentification)
    The changes are in 4 Files

--- src/main/java/lsit/Config.java
+++ src/main/java/lsit/Config.java
@@ -1,31 +1,32 @@
-// package lsit;
+package lsit;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
-// import static org.springframework.security.config.Customizer.withDefaults;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-// @Configuration
-// @EnableWebSecurity
-// public class Config {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-//     // need to change user path to our paths
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//         http
-//             .csrf(c -> c.disable())
-//             .oauth2Login(withDefaults())
-//             .authorizeHttpRequests(a -> a
-//                 .requestMatchers("/user").authenticated()
-//                 .anyRequest().permitAll()
-//             )
-//             ;
-//         return http.build();
+@Configuration
+@EnableWebSecurity
+public class Config{
 
-//     }
-        
-// }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(c -> c.disable())
+            .oauth2Login(withDefaults())
+            .authorizeHttpRequests(a -> a
+                .requestMatchers("/user").authenticated()
+                .requestMatchers(HttpMethod.POST, "/pets").authenticated()
+                .anyRequest().permitAll()
+            )
+            ;
 
+        return http.build();
+    }
+
+}
--- src/main/java/lsit/Controllers/HomeController.java
+++ src/main/java/lsit/Controllers/HomeController.java
@@ -1,6 +1,9 @@
 package lsit.Controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +13,28 @@ public class HomeController {
     @GetMapping("/")
     public ResponseEntity get(){
         return ResponseEntity.ok("Hello World!");
+    }
+
+    @GetMapping("/user")
+    public String getUser(OAuth2AuthenticationToken authentication){
+        // var groups = (List<String>)authentication.getPrincipal().getAttribute("groups");
+        // return groups.get(0);
+
+        var userAttributes = authentication.getPrincipal().getAttributes();
+
+        // StringBuilder sb = new StringBuilder();
+        // for(var entry : userAttributes.entrySet()){
+        //     var s = entry.getKey() + ": " + entry.getValue();
+        //     sb.append("\n").append(s);
+        // }
+
+        return "<pre> \n" +
+            userAttributes.entrySet().parallelStream().collect(
+                StringBuilder::new,
+                (s, e) -> s.append(e.getKey()).append(": ").append(e.getValue()),
+                (a, b) -> a.append("\n").append(b)
+            ) +
+            "</pre>";
     }
 
 }
