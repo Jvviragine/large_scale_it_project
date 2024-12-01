ae78b0a7adccb7621eed31b22a7b0d91ff5a09d8
Weronika Górak <weronika.gorak@gmail.com>
2024-12-01 20:25:32 +0100

    changes

--- src/main/java/lsit/Controllers/PaymentController.java
+++ src/main/java/lsit/Controllers/PaymentController.java
@@ -23,21 +23,21 @@ public class PaymentController {
 
     // access: manager
     @GetMapping("/payments")
-    @PreAuthorize("hasAnyRole('PIZZARIA_MANAGER')")
+    @PreAuthorize("hasAnyRole('PIZZERIA_MANAGER')")
     public List<Payment> getAllPayments(){
         return paymentRepository.list();
     }
 
     // access: manager, server
     @GetMapping("/payments/{id}")
-    @PreAuthorize("hasAnyRole('PIZZARIA_SERVER', 'PIZZARIA_MANAGER')")
+    @PreAuthorize("hasAnyRole('PIZZERIA_SERVER', 'PIZZERIA_MANAGER')")
     public Payment getPaymentById(@PathVariable("id") UUID PaymentId){
         return paymentRepository.get(PaymentId);
     }
 
     // access: server, manager
     @PostMapping("/payments")
-    @PreAuthorize("hasAnyRole('PIZZARIA_SERVER', 'PIZZARIA_MANAGER')")
+    @PreAuthorize("hasAnyRole('PIZZERIA_SERVER', 'PIZZERIA_MANAGER')")
     public Payment addPayment(@RequestBody Payment p){
         paymentRepository.add(p);
         return p;
@@ -45,7 +45,7 @@ public class PaymentController {
 
     // access: manager
     @PutMapping("/payments/{id}")
-    @PreAuthorize("hasAnyRole('PIZZARIA_MANAGER')")
+    @PreAuthorize("hasAnyRole('PIZZERIA_MANAGER')")
     public Payment updatePayment(@PathVariable("id") UUID PaymentId, @RequestBody Payment p){
         p.setPaymentId(PaymentId);
         paymentRepository.update(p);
@@ -54,7 +54,7 @@ public class PaymentController {
 
     // access: manager
     @DeleteMapping("/payments/{id}")
-    @PreAuthorize("hasAnyRole('PIZZARIA_MANAGER')")
+    @PreAuthorize("hasAnyRole('PIZZERIA_MANAGER')")
     public void deletePayment(@PathVariable("id") UUID PaymentId){
         paymentRepository.remove(PaymentId);
     }
--- src/main/java/lsit/Repositories/PaymentRepository.java
+++ src/main/java/lsit/Repositories/PaymentRepository.java
@@ -4,7 +4,6 @@ import java.util.*;
 import org.springframework.stereotype.Repository;
 import lsit.Models.Payment;
 
-
 @Repository
 public class PaymentRepository extends GenericS3Repository<Payment>  {
     public PaymentRepository() {
